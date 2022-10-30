81871adaa207841ab43b60ed74d05c7c9b6f98e7
Fabrice <57037980+Fabricefabfab@users.noreply.github.com>
2022-10-30 13:09:15 +0200

    Update connn.php

--- Controller/connn.php
+++ Controller/connn.php
@@ -2,12 +2,21 @@
 <?php
 
 require "database_credential.php";
-// Create connection
-$conn = new mysqli(servename, username, password, db);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+//check connection
+if(!$conn){
+	die("connection failed");
 }
 
 ?>
